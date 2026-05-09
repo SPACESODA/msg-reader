7ee6abac9ad41039fc7bc2945ee81cbde5c1109a
Anthony C. <anthony.chan.2509@gmail.com>
2026-05-10 08:03:42 +0900

    initial commit

---
+++ messages.js
@@ -0,0 +1,62 @@
+const messagesData = {
+  "title": "Demo Messages",
+  "highlighted_senders": "JS Tester",
+  "messages": [
+    {
+      "message_id": 1,
+      "timestamp_iso": "2026-05-10T11:00:00+09:00",
+      "sender": "JS Tester",
+      "reply_to": null,
+      "text": "Hello! This is message 1 from the JS file."
+    },
+    {
+      "message_id": 2,
+      "timestamp_iso": "2026-05-10T11:05:00+09:00",
+      "sender": "System",
+      "reply_to": null,
+      "text": "This is message 2. The standard script loader is working."
+    },
+    {
+      "message_id": 3,
+      "timestamp_iso": "2026-05-10T11:10:00+09:00",
+      "sender": "User A",
+      "reply_to": null,
+      "text": "Message 3 here! Checking the JS timeline."
+    },
+    {
+      "message_id": 4,
+      "timestamp_iso": "2026-05-10T11:15:00+09:00",
+      "sender": "JS Tester",
+      "reply_to": 3,
+      "text": "Replying to message 3 in JS. This is message 4."
+    },
+    {
+      "message_id": 5,
+      "timestamp_iso": "2026-05-10T11:20:00+09:00",
+      "sender": "System",
+      "reply_to": null,
+      "text": "Message 5 from the JS script array."
+    },
+    {
+      "message_id": 6,
+      "timestamp_iso": "2026-05-10T11:25:00+09:00",
+      "sender": "User B",
+      "reply_to": null,
+      "text": "This is message 6 in the JS dummy."
+    },
+    {
+      "message_id": 7,
+      "timestamp_iso": "2026-05-10T11:30:00+09:00",
+      "sender": "JS Tester",
+      "reply_to": null,
+      "text": "Message 7, another highlighted JS sender."
+    },
+    {
+      "message_id": 8,
+      "timestamp_iso": "2026-05-10T11:35:00+09:00",
+      "sender": "System",
+      "reply_to": null,
+      "text": "Finally, message 8. The dummy JS data is complete."
+    }
+  ]
+};
